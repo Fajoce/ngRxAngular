e3e2ed095840b5509b4ce8db9728e42a3ee1e6b1
fajoce <fajoce@gmail.com>
2025-07-04 10:37:58 -0500

    Agregue Paginacion y filtros al componente lsitar productos

--- src/app/components/list-products/list-products.component.ts
+++ src/app/components/list-products/list-products.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { Observable } from 'rxjs';
 import { Product } from '../../models/product';
@@ -9,6 +9,10 @@ import { Router } from '@angular/router';import { MatTableModule } from '@angula
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatTableDataSource } from '@angular/material/table';
+import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-list-products',
@@ -18,23 +22,42 @@ import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
     MatTableModule,
     MatButtonModule,
     MatIconModule,
-    MatProgressSpinnerModule
+    MatProgressSpinnerModule,
+    MatPaginatorModule,
+    MatFormFieldModule,
+    MatInputModule
   ],
   templateUrl: './list-products.component.html',
   styleUrl: './list-products.component.css'
 })
 export class ListProductsComponent implements OnInit {
   displayedColumns: string[] = ['id','name', 'price', 'category', 'actions'];
-  products$: Observable<Product[]>;
+  dataSource = new MatTableDataSource<Product>();
   error$: Observable<string | null>;
+  showFilter = false;
+
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
 
   constructor(private store: Store, private router: Router) {
-    this.products$ = this.store.select(fromProduct.selectAllProducts);
     this.error$ = this.store.select(fromProduct.selectProductError);
   }
 
   ngOnInit(): void {
     this.store.dispatch(ProductActions.loadProducts());
+
+    this.store.select(fromProduct.selectAllProducts).subscribe(products => {
+      this.dataSource.data = products;
+      this.dataSource.paginator = this.paginator;
+    });
+  }
+
+  toggleFilter() {
+    this.showFilter = !this.showFilter;
+  }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
   }
 
   edit(prod: Product) {
